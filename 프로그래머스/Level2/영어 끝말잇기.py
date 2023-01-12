fdc7d28fe1cbc2f046f03d2c77e1a10a2779f6f4
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-01-13 01:43:03 +0900

    Add files via upload

---
+++ 프로그래머스/Level2/영어 끝말잇기.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(n, words):
+    num = 0
+    count = 0
+    first_word = words[0]
+    
+    dq = deque()
+    
+    for i in words:
+        print(dq)
+        num+=1
+        if num % n == 1:
+            num = 1
+            count+=1
+            
+        if len(dq)==0:
+            last = i[-1]
+            dq.append(i)
+            continue
+        elif i in dq:
+            return [num,count]
+        else:
+            first = i[0]
+            if last != first:
+                return [num,count]
+            else:
+                last = i[-1]
+                dq.append(i)
+
+    return [0,0]
