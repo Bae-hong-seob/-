355e736648d2ba683f522b01bc6377cb6e4fe797
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-01-19 07:23:41 +0900

    Add files via upload

--- 프로그래머스/Level2/점프와 순간 이동.py
+++ 프로그래머스/Level2/점프와 순간 이동.py
@@ -1,9 +1,22 @@
+# def solution(n):
+#     answer = 1
+#     while n != 1:
+#         if n % 2 == 0:
+#             n /= 2
+#         else:
+#             answer += 1
+#             n -= 1
+#     return answer
+
+
 def solution(n):
-    answer = 1
-    while n != 1:
-        if n % 2 == 0:
-            n /= 2
+    
+    answer=1
+    
+    while(n!=1):
+        if n % 2 == 1:
+            n-=1
+            answer+=1
         else:
-            answer += 1
-            n -= 1
+            n/=2
     return answer
