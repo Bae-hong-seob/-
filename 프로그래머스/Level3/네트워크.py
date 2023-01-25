0a337e772ac8c794dbddc25b17ae603baee201ec
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-01-25 14:59:08 +0900

    Update 네트워크.py

--- 프로그래머스/Level3/네트워크.py
+++ 프로그래머스/Level3/네트워크.py
@@ -1,18 +1,22 @@
 def solution(n, computers):
     
-    def dfs(v):
-        visited[v] = True
-
-        for nei in range(n):    # 인접노드 탐구 
-            if not visited[nei] and computers[v][nei]:    # unvisited + 인접할 때 
-                dfs(nei)
+    visited = [False]*n
+    count = 0  
+    
+    def dfs(node):
+        visited[node] = True
         
-    count = 0   
-    visited = [False] * (n)   
-
-    for node_idx in range(n):
-        if not visited[node_idx] :
+        for neighbor in range(n): #인접 노드 탐색
+            if visited[neighbor] == False and computers[node][neighbor] == 1: #방문 X and 인접노드 일 때 
+                dfs(neighbor) 
+            else:
+                continue
+    
+    for node_idx in range(n): #모든 노드에 대해 반복 검사
+        if visited[node_idx] == False:
             dfs(node_idx)
-            count += 1         # dfs 끝나면 count 해주기 
-
+            count+=1
+        else:
+            continue
+    
     return count
