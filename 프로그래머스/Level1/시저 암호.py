bfa0853faf6eae9f7b8bedd7e02b01b30316f61c
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-01-26 09:32:34 +0900

    Add files via upload

--- 프로그래머스/Level1/시저 암호.py
+++ 프로그래머스/Level1/시저 암호.py
@@ -1,12 +1,11 @@
 def solution(s, n):
     answer = ''
     for i in s:
-        if i:
-            if i >= 'A' and i <= 'Z':
-                answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
-            elif i >= 'a' and i <= 'z':
-                answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
-            else : answer += ' '
+        if i >= 'A' and i <= 'Z':
+            answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+        elif i >= 'a' and i <= 'z':
+            answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
+        else : answer += ' '
     return answer
 
 # def solution(s, n):
