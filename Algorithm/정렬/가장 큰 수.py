45c77af3e83c0c7d611ed19b760fac190434a31d
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-02-14 16:14:24 +0900

    Add files via upload

--- Algorithm/정렬/가장 큰 수.py
+++ Algorithm/정렬/가장 큰 수.py
@@ -1,5 +1,6 @@
 def solution(numbers):
     numbers = list(map(str,numbers))
-    numbers.sort(key=lambda x : str(x)*3, reverse=True)
+    numbers.sort(reverse=True)
+    numbers.sort(key=lambda x : x*3, reverse=True)
     
     return "0" if numbers[0]=="0" else ''.join(numbers)
