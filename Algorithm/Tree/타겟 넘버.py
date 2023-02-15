4a12c6b861dfb4d6bb2407ecc9750e1d01f3390c
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-02-15 11:40:51 +0900

    Add files via upload

--- Algorithm/Tree/타겟 넘버.py
+++ Algorithm/Tree/타겟 넘버.py
@@ -1,4 +1,4 @@
-# 너비 우선 탐색(queue)
+# 너비 우선 탐색(queue) -> leaf node만 남김
 from collections import deque
 def solution(numbers, target):
     tree = deque()
@@ -16,3 +16,27 @@ def solution(numbers, target):
         n+=1
     answer = 0
     return tree.count(target)
+
+# 너비 우선 탐색 - 트리를 전체를 구현
+# from collections import deque 
+
+# def solution(numbers, target):
+    
+#     q = deque()
+#     count=0
+#     n = len(numbers)
+    
+#     q.append([numbers[0],0])
+#     q.append([-numbers[0],0])
+    
+#     while q:
+#         value, idx = q.popleft()
+#         idx+=1
+#         if idx < n:
+#             q.append([value+numbers[idx],idx])
+#             q.append([value-numbers[idx],idx])
+#         else:
+#             if value == target:
+#                 count+=1 
+
+#     return count
