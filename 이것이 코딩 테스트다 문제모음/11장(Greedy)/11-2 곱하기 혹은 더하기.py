194bdf19658778f4f5d410f08b52fa5e951984c9
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-07-02 14:06:17 +0900

    Add files via upload

---
+++ 이것이 코딩 테스트다 문제모음/11장(Greedy)/11-2 곱하기 혹은 더하기.py
@@ -0,0 +1,12 @@
+N = '01010011'
+
+N_list = list(map(int,N))
+
+answer=0
+for num in N_list:
+            if num==0 or num==1 or answer==0:
+                        answer+=num
+            else:
+                        answer*=num
+
+print(answer)
---
+++ 이것이 코딩 테스트다 문제모음/11장(Greedy)/11-3 문자열 뒤집기.py
@@ -0,0 +1,18 @@
+#N = '0001100'
+N = input()
+
+make_0_count = N.split('0')
+make_0 = 0 
+for count in make_0_count:
+            if count != '':
+                        make_0+=1
+print(make_0)
+                        
+make_1_count = N.split('1')
+make_1 = 0 
+for count in make_1_count:
+            if count != '':
+                        make_1+=1
+print(make_1)
+
+print(min(make_0, make_1))
