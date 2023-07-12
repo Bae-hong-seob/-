c7801e908081329f5791e5e2755f133d086bd3fd
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-07-12 12:59:21 +0900

    Add files via upload

---
+++ 이것이 코딩 테스트다 문제모음/5장(DFS,BFS)/5-10 음료수 얼려 먹기.py
@@ -0,0 +1,35 @@
+N, M = 4, 5 # N은 세로길이, M은 가로길이
+graph = [
+    [0,0,1,1,0],
+    [0,0,0,1,1],
+    [1,1,1,1,1],
+    [0,0,0,0,0]
+]
+
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x,y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        
+        # 상, 하, 좌, 위의 위치도 모두 재귀적으로 호출
+        dfs(x-1,y)
+        dfs(x,y-1)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        return True
+    return False
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(N):
+    for j in range(M):
+        # 현재 위치에서 DFS 수행
+        if dfs(i,j) == True:
+            result+=1
+            
+print(result)
