a9b3cbbd4345eb7559f578db6064d38eacee31b9
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2023-10-08 16:07:14 +0900

    Delete 프로그래머스/고득점kit/4. 정렬 directory

--- 프로그래머스/고득점kit/4. 정렬/H-index.py
+++
@@ -1,7 +0,0 @@
-def solution(citations):
-    citations.sort()
-    for idx , citation in enumerate(citations): #enumerate 는 list의 index와 value를 반환.
-        print(idx, citation, citations)
-        if citation >= len(citations) - idx :
-            return len(citations) - idx
-    return 0
--- 프로그래머스/고득점kit/4. 정렬/K번째 수.py
+++
@@ -1,9 +0,0 @@
-def solution(array, commands):
-    answer = []
-
-    for i,j,k in commands:
-        candidate = array[i-1:j]
-        candidate.sort()
-        answer.append(candidate[k-1])
-        
-    return answer
