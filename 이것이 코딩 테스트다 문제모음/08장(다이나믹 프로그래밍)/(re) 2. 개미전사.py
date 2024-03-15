e861e200bde822f5f2dd047895a4746f65e826f6
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2024-03-15 22:36:19 +0900

    Update (re) 2. 개미전사.py

--- 이것이 코딩 테스트다 문제모음/08장(다이나믹 프로그래밍)/(re) 2. 개미전사.py
+++ 이것이 코딩 테스트다 문제모음/08장(다이나믹 프로그래밍)/(re) 2. 개미전사.py
@@ -2,7 +2,7 @@ N = 4
 stores = [1,3,1,5]
 
 dp_table = [0]*N
-dp_table[0], dp_table[1] = stores[0], stores[1]
+dp_table[0], dp_table[1] = stores[0], max(stores[0],stores[1])
 
 for i in range(2,N):
     dp_table[i] = max(dp_table[i-2]+stores[i], dp_table[i-1])
