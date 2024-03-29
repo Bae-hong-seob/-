1bd152441b42c3e2693ac4af76e893eb4b621211
Bae-hong-seob <49437396+Bae-hong-seob@users.noreply.github.com>
2024-03-29 15:51:11 +0900

    Add files via upload

---
+++ baekjoon_Algorithm 문제모음/백트래킹/N과 M (4).py
@@ -0,0 +1,17 @@
+N,M = 3,1
+N,M = 4,2
+N,M = 3,3
+N,M = map(int, input().split())
+
+def dfs(stack):
+    if len(stack)==M:
+        for i in stack:
+            print(i, end=' ')
+        print()
+        return
+    
+    for next in range(stack[-1], N+1):
+        dfs(stack+[next])
+        
+for i in range(1,N+1):
+    dfs([i])
